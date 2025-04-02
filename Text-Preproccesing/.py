4c18f6d49d568dd4cbed09c20ecfca293f8744bd
HuyBao (Harrywyw) <10421020@student.vgu.edu.vn>
2025-04-02 11:43:34 +0700

    Update and rename CTR.py to .py

--- Text-Preproccesing/.py
+++ Text-Preproccesing/.py
@@ -115,17 +115,3 @@ sheet = book.create_sheet(title=new_sheet_name)
 for r in dataframe_to_rows(filtered_df, index=False, header=True):
     sheet.append(r)
 book.save(existing_file_path)
-/project_root
- ├── data_processing/
- │   ├── load_data.py
- │   ├── save_data.py
- ├── preprocessing/
- │   ├── normalize_text.py
- │   ├── remove_frequent_rare_words.py
- ├── feature_extraction/
- │   ├── frequency_vector.py
- │   ├── tfidf_vectorizer.py
- │   ├── word_embeddings.py
- ├── keyword_grouping/
- │   ├── group_keywords.py
- ├── main.py
